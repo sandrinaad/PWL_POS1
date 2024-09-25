114facd5bf589b7cca987fe7ef87bd0877ace7d2
sandrinaad <sandrina.bridge@gmail.com>
2024-09-25 10:58:52 +0700

    Praktikum 2 JS5

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,43 +16,44 @@
 <!-- Site wrapper -->
 <div class="wrapper">
   <!-- Navbar -->
-  @include(layouts.header)
+ @include('layouts.header')
   <!-- /.navbar -->
+
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/')}}" class="brand-link">
-      <img src="{{asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">PWL - Starter Code</span>
     </a>
 
     <!-- Sidebar -->
-    @include(layouts.sidebar)
+@include('layouts.sidebar')
     <!-- /.sidebar -->
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    @include(layouts.breadcrumb)
+    @include('layouts.breadcrumb')
 
     <!-- Main content -->
     <section class="content">
-        @yield('content')
+    @yield('content')
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
 
-  @include(layouts.footer)
+@include('layouts.footer')
 </div>
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{asset ('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{asset ('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{asset ('adminlte/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\levelcontroller;
 use App\Http\Controllers\kategoricontroller;
 use App\Http\Controllers\usercontroller;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/level',[levelcontroller::class, 'index']);
-Route::get('/kategori',[KategoriController::class, 'index']);
-Route::get('/user',[UserController::class, 'index']);
-Route::get('/user/tambah',[UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [usercontroller::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);           
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+use App\Http\Controllers\WelcomeController;
+
+
+Route::get('/', [WelcomeController::class, 'index']);
