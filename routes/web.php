98e449134552adb1f15bd4d56febd5bff9fed61b
sandrinaad <sandrina.bridge@gmail.com>
2024-10-01 19:45:45 +0700

    Jobsheet 5 Praktikum 3

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\levelcontroller;
-use App\Http\Controllers\kategoricontroller;
 use App\Http\Controllers\usercontroller;
 use App\Http\Controllers\WelcomeController;
 
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [usercontroller::class, 'index']);          //menmapilkan halaman awal user
+    Route::post('/list', [usercontroller::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
+    Route::get('/create', [usercontroller::class, 'create']);   //menampilkan halaman form tambah user
+    Route::post('/', [usercontroller::class, 'store']);         //menyimpan data user baru
+    Route::get('/{id}', [usercontroller::class, 'show']);       //menampilkan detail user
+    Route::get('/{id}/edit', [usercontroller::class, 'edit']);  //menampilkan halaman form edit user
+    Route::put('/{id}', [usercontroller::class, 'update']);     //menyimpan perubahan data user
+    Route::delete('/{id}', [usercontroller::class, 'destroy']); //menghapus data user
+});
