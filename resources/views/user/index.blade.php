3544ecf5c8b0f2a63c92c604c0ee1037482d60d1
sandrinaad <sandrina.bridge@gmail.com>
2024-10-02 11:40:22 +0700

    Praktikum 1 JS 6

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.template')
 @section('content')
-<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div> 
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" 
+    data-keyboard="false" data-width="75%" aria-hidden="true"></div>
     <div class="card card-outline card-primary">
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
-                <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-s, btn-success mt-1">Tambah Ajax</button>
+                <button onclick="modalAction('{{ url('user/create_ajax')}}')" class="btn btn-success mt-1">Tambah Ajax</button>
             </div>
         </div>
         <div class="card-body">
@@ -19,11 +20,11 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="form-group row">
-                        <label class="col-1 control-label col-form-label" for="level_id">Filter:</label>
+                        <label class="col-1 control-label col-form-label">Filter:</label>
                         <div class="col-3">
                             <select class="form-control" id="level_id" name="level_id" required>
                                 <option value="">- Semua -</option>
-                                @foreach($level as $item)
+                                @foreach ($level as $item)
                                     <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
                                 @endforeach
                             </select>
@@ -50,13 +51,14 @@
 @endpush
 @push('js')
     <script>
-        function modalAction(url = ''){ 
-            $('#myModal').load(url,function(){ 
-                $('#myModal').modal('show'); 
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
             });
         }
+        var dataUser;
         $(document).ready(function() {
-            var dataUser = $('#table_user').DataTable({
+             dataUser = $('#table_user').DataTable({
                 // serverSide: true, jika ingin menggunakan server side processing
                 serverSide: true,
                 ajax: {
@@ -64,7 +66,7 @@
                     "dataType": "json",
                     "type": "POST",
                     "data": function(d){
-                        d.level_id = $('#level_id').val();
+                        d.level_id=$('#level_id').val();
                     }
                 },
                 columns: [{
@@ -98,9 +100,8 @@
                     searchable: false
                 }]
             });
-
             $('#level_id').on('change', function(){
-                dataUser.ajax.reload();
+              dataUser.ajax.reload();
             });
         });
     </script>
