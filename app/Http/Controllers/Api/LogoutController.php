36527f0e60d464bfbfbd0e2cee1aef1dd7b6d3ba
sandrinaad <sandrina.bridge@gmail.com>
2024-11-05 23:29:42 +0700

    Jobsheet 10 Praktikum 3

--- app/Http/Controllers/Api/LogoutController.php
+++ app/Http/Controllers/Api/LogoutController.php
@@ -1,23 +1,18 @@
 <?php
-
 namespace App\Http\Controllers\Api;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
-
 class LogoutController extends Controller
 {
     public function __invoke(Request $request)
     {
-        //remove token
         $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
-
-        if($removeToken){
-            //return reponse JSON
+        if ($removeToken) {
             return response()->json([
-                'success' => true,
-                'message' => 'Logout Berhasil',
+                'success'   => true,
+                'message'   => 'Logout Berhasil',
             ]);
         }
     }
